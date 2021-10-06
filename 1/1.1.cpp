fefcc3e2c01d02b9bf0cf09cb48abdd4845e54ef
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-10-06 20:20:48 +0300

    Add files via upload

---
+++ 1/1.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A, B, C;
+	cout << " А ";
+		cin >> A;
+	cout << " B ";
+		cin >> B;
+	cout << " C ";
+		cin >> C;
+
+		if (A < B && B < C) {
+			cout << "OK";
+		}
+		else {
+			cout << "NOT OK";
+		}
+			return 0;
+}
