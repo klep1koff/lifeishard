876ffdab69f8c717a27f30a50a06bd4a38b2ee2d
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-10-12 20:28:39 +0300

    Add files via upload

---
+++ 4/4.1.cpp
@@ -0,0 +1,28 @@
+﻿#include "iostream"
+using namespace std;
+
+int main()
+{
+	int m[1000], n;
+	cout << "enter amount of numbers ";
+	cin >> n;
+	cout << "enter elements of array " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m[i];
+	}
+	for (int i = 1; i < n; i++) {
+		if ((m[i] > 0 && m[i - 1] > 0) || (m[i] < 0 && m[i - 1] < 0)) {
+
+
+			cout << endl << m[i] << endl << "999" << endl << m[i - 1] << endl;
+		}
+	}
+
+	return 0;
+}
+
+
+
+
+
