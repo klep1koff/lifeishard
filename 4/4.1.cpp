21a7d1f55f743c47275b52287d00c4d9a8e0b5cd
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-13 21:47:46 +0300

    Update 4.1.cpp

--- 4/4.1.cpp
+++ 4/4.1.cpp
@@ -1,23 +1,17 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
-
 int main()
 {
-	int m[1000], n;
-	cout << "enter amount of numbers ";
-	cin >> n;
-	cout << "enter elements of array " << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> m[i];
-	}
-	for (int i = 0; i < n; i++) {
-		cout << m[i] << endl;
-		if ((m[i] > 0 && m[i + 1] > 0) || (m[i] < 0 && m[i + 1] < 0)) {
-			cout << 999 << endl;
-		}
-	}
-
-
-	return 0;
+    int m[4] = {1,-1,3,4};
+    int n = sizeof(m)/sizeof(m[0]);
+    for(int i=0; i < n; i++)
+        
+      if ((m[i] > 0 && m[i + 1] > 0) || (m[i] < 0 && m[i + 1] < 0)) {
+			cout<< m[i] << "," << 999 << "," ;
+        }
+        
+        else {
+            cout << m[i] << ",";
+        }
+    return 0;
 }
