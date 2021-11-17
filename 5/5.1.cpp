02d0cd9d2d50167ad9413aec007714c760735c2d
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-17 13:21:32 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -1,108 +1,28 @@
-﻿#include <iostream>
-using namespace std;
-
-int
-main()
-{
-	int a[3] = { 1, 8, 3 };
-	int sum1 = 0;
-	int m = sizeof(a) / sizeof(a[0]);
-	for (int i = 0; i < m; i++)
-	{
-		cout << a[i];
-	}
-	cout << endl;
-	for (int i = 0; i < m; i++)
-	{
-		if (a[i] % 2 == 0)
-		{
-			sum1 = sum1 + a[i];
-		}
-
-	}
-
-
-	int b[3] = { 1, 2, 3 };
-	int sum2 = 0;
-	int n = sizeof(b) / sizeof(b[0]);
-	for (int i = 0; i < n; i++)
-	{
-		cout << b[i];
-	}
-	cout << endl;
-	for (int i = 0; i < n; i++)
-	{
-		if (b[i] % 2 == 0)
-		{
-			sum2 = sum2 + b[i];
-		}
-
-	}
-
-
-	int c[3] = { 1, 2, 3 };
-	int sum3 = 0;
-	int k = sizeof(c) / sizeof(c[0]);
-	for (int i = 0; i < k; i++)
-	{
-		cout << c[i];
-	}
-	cout << endl;
-	for (int i = 0; i < k; i++)
-	{
-		if (c[i] % 2 == 0)
-		{
-			sum3 = sum3 + c[i];
-		}
-
-	}
-
-	cout << endl << sum1 << endl << sum2 << endl << sum3 << endl;
-
-	/*
-	if ((sum1>sum2)&&(sum2>sum3))
-	 cout <<  sum1 << " - max";
-
-	else if ((sum2>sum1)&&(sum2>sum3))
-	cout << sum2 << " - max";
-
-	else if ((sum3>sum1)&&(sum3>sum2))
-	cout << sum3 << " - maksimalnoe.";
-
-	else (sum1==sum2==sum3);
-	cout << " - equal";
-	*/
-
-}
-
-
-/*#include"iostream"
+#include<iostream>
 using namespace std;
 int
 main ()
 {
-  int n, m;
-  cout << "Enter number of lines : "; // i //
-  cin >> n;
-  cout << "Enter number of columns : "; // j //
-  cin >> m;
-  int a[n][m];
-  cout << "Enter elements of array : " << endl;
-  for (int i = 0; i < n; i++) 
+  int a[3][3], m;
+  for (int i = 0; i < 3; i++)
     {
-      for (int j = 0; j < m; j++)
- {
-   cout << "element [" << i+1 << "][" << j+1 << "] = ";
-   cin >> a[i][j];
- }
+      for (int j = 0; j < 3; j++)
+	{
+	  cout << "element [" << i + 1 << "][" << j + 1 << "] = ";
+	  cin >> a[i][j];
+	}
     }
-  cout << "array :" << endl;
-  for (int i = 0; i < n; i++)
+  cout << endl;
+
+
+  for (int i = 0; i < 3; i++)
     {
-      for (int j = 0; j < m; j++)
- cout << a[i][j] << " ";
-      cout << endl;
+      
+      for (int j = 1; j < 3; j++)
+	  }
+      cout << "max summ =  " << maxsumm ;
+      m = 0;
     }
+
   return 0;
 }
-*/
