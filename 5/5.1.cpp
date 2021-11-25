828f06e6d7cc045eecbb1a98f45bc023480480a3
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-25 13:23:28 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -3,26 +3,24 @@ using namespace std;
 int
 main ()
 {
+  int maxsumm,i , j, k;
+  
   int a[3][3], m;
-  for (int i = 0; i < 3; i++)
+    for (i = 0; i < 3; i++)
     {
-      for (int j = 0; j < 3; j++)
+      for (j = 0; j < 3; j++)
 	{
 	  cout << "element [" << i + 1 << "][" << j + 1 << "] = ";
 	  cin >> a[i][j];
+	  
+	if (a % 2 == 0) {
+	     
+	}
 	}
     }
+    
+	     
   cout << endl;
 
-
-  for (int i = 0; i < 3; i++)
-    {
-      // тут не могу додуплить,  что надо вставить. пока что вариантов нет(( //
-      for (int j = 1; j < 3; j++)
-	  }
-      cout << "max summ =  " << maxsumm ;
-      m = 0;
-    }
-
-  return 0;
+return 0;
 }
