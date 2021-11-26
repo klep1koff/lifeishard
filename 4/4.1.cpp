ba351b83c10ae784f8b936e7485256b565f9f396
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-26 22:05:28 +0300

    Update 4.1.cpp

--- 4/4.1.cpp
+++ 4/4.1.cpp
@@ -3,17 +3,27 @@ using namespace std;
 int main()
 {
     int m[4] = {-1,2,3,4};
-    int n = sizeof(m)/sizeof(m[0]);
-    for(int i=0; i < n; i++)
+    int b[4]; 
+    int i, n;
+    n=4;
+    for(i=0; i < n; i++)
         {
-      if (m[i] * m[i + 1] > 0) 
+      cout << m[i];
+        }
+        
+    cout << endl;    
+        
+    for (i=0; i < n; i++) 
+     {
+         b[i] = m[i];
+         if (b[i] * b[i + 1] > 0) 
       { 
-			cout<< m[i] << "," << 999 << "," ;
+			cout<< b[i] << "," << 999 << "," ;
         }
         
         else {
-            cout << m[i] << ",";
+            cout << b[i] << ",";
         }
-        }
+     }  
     return 0;
 }
