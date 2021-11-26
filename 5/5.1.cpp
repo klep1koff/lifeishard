9ec2f77df9be16d8bb91419e8b887ae45f2e7562
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-26 21:50:02 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int
 main ()
 {
-  int maxsumm,i , j, k;
+  int i , j, sum1, sum2, sum3;
   
   int a[3][3], m;
     for (i = 0; i < 3; i++)
@@ -12,15 +12,29 @@ main ()
 	{
 	  cout << "element [" << i + 1 << "][" << j + 1 << "] = ";
 	  cin >> a[i][j];
-	  
-	if (a % 2 == 0) {
-	     
-	}
-	}
+	    }
     }
-    
-	     
-  cout << endl;
-
-return 0;
+   
+	    sum1 = 0;
+	    sum2 = 0;
+	    sum3 = 0;
+        for (i = 0; i < 3; i++) 
+            for (j = 0; j < 3; j++)
+        {       
+            if (a[i][j] % 2 == 0)
+                {
+                    sum1 += a[i][j];
+                }
+            else if (a[i][j] % 2 == 0)
+                {
+                    sum2 += a[i][j];
+                }  
+            else if (a[i][j] % 2 == 0)
+                {
+                    sum3 += a[i][j];
+                }    
+        }
+        cout << " summ 1 = "  <<  sum1 << " summ 2 = "  <<  sum2<< " summ 2 =  "  <<  sum3; 
+     cout << endl;
+ return 0; 
 }
