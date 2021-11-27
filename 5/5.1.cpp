aeca5638d63cfb915088931182be522ede255b01
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-27 17:57:23 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -18,20 +18,19 @@ main ()
 	    sum1 = 0;
 	    sum2 = 0;
 	    sum3 = 0;
-        for (i = 0; i < 3; i++) 
             for (j = 0; j < 3; j++)
         {       
-            if (a[i][j] % 2 == 0)
+            if (a[0][j] % 2 == 0)
                 {
-                    sum1 += a[i][j];
+                    sum1 += a[0][j];
                 }
-            else if (a[i][j] % 2 == 0)
+            else if (a[1][j] % 2 == 0)
                 {
-                    sum2 += a[i][j];
+                    sum2 += a[1][j];
                 }  
-            else if (a[i][j] % 2 == 0)
+            else if (a[2][j] % 2 == 0)
                 {
-                    sum3 += a[i][j];
+                    sum3 += a[2][j];
                 }    
         }
         cout << " summ 1 = "  <<  sum1 << " summ 2 = "  <<  sum2<< " summ 2 =  "  <<  sum3; 
