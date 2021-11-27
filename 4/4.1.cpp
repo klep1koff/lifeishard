daa402f2042d320f0e54b459518c0e2ad22fe013
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-27 19:01:21 +0300

    Update 4.1.cpp

--- 4/4.1.cpp
+++ 4/4.1.cpp
@@ -2,28 +2,65 @@
 using namespace std;
 int main()
 {
-    int m[4] = {-1,2,3,4};
-    int b[4]; 
-    int i, n;
+    int m[4], b[4], i, n;
     n=4;
     for(i=0; i < n; i++)
         {
-      cout << m[i];
+      cout << "enter element [" << i + 1 << "] : ";
+      cin >> m[i];
         }
         
-    cout << endl;    
+    cout << endl;
+    
         
     for (i=0; i < n; i++) 
-     {
-         b[i] = m[i];
-         if (b[i] * b[i + 1] > 0) 
+        b[i] = m[i];
+     
+       
+    for (i=0; i < n; i++)
+    {
+    if (b[i] * b[i + 1] > 0)
+    
       { 
 			cout<< b[i] << "," << 999 << "," ;
-        }
+      }
         
-        else {
+    else {
             cout << b[i] << ",";
-        }
-     }  
+         }
+    }    
+    return 0;
+}
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m[4], b[4], i, n;
+    n=4;
+    for(i=0; i < n; i++)
+        {
+      cout << "enter element [" << i + 1 << "] : ";
+      cin >> m[i];
+      b[i] = m[i];
+        }   
+    cout << endl;
+       
+    for (i=0; i < n; i++)
+    {
+    if (b[i] * b[i + 1] > 0)
+    
+      { 
+	cout<< b[i] << "," << 999 << "," ;
+      }
+        
+    else {
+            cout << b[i] << ",";
+         }
+    }    
     return 0;
 }
