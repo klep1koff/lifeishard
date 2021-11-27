ed149154ac2b82655df8921fe97ee651269773c2
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-27 18:48:05 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 using namespace std;
-int
-main ()
+int main ()
 {
   int i , j, sum1, sum2, sum3;
   
-  int a[3][3], m;
+  int a[3][3];
     for (i = 0; i < 3; i++)
     {
       for (j = 0; j < 3; j++)
