079f30d110ece438d0f3a8b03cedcfe10de415e9
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-11-27 19:15:31 +0300

    Update 4.1.cpp

--- 4/4.1.cpp
+++ 4/4.1.cpp
@@ -2,46 +2,7 @@
 using namespace std;
 int main()
 {
-    int m[4], b[4], i, n;
-    n=4;
-    for(i=0; i < n; i++)
-        {
-      cout << "enter element [" << i + 1 << "] : ";
-      cin >> m[i];
-        }
-        
-    cout << endl;
-    
-        
-    for (i=0; i < n; i++) 
-        b[i] = m[i];
-     
-       
-    for (i=0; i < n; i++)
-    {
-    if (b[i] * b[i + 1] > 0)
-    
-      { 
-			cout<< b[i] << "," << 999 << "," ;
-      }
-        
-    else {
-            cout << b[i] << ",";
-         }
-    }    
-    return 0;
-}
-
-
-
-
-
-#include <iostream>
-using namespace std;
-int main()
-{
-    int m[4], b[4], i, n;
-    n=4;
+    int m[4], b[4], i, n = 4;
     for(i=0; i < n; i++)
         {
       cout << "enter element [" << i + 1 << "] : ";
