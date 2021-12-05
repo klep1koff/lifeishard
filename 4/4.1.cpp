69ce71f97cd7199d1f3fac447d173f38a6ff369e
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-05 23:00:02 +0300

    Update 4.1.cpp

--- 4/4.1.cpp
+++ 4/4.1.cpp
@@ -2,26 +2,31 @@
 using namespace std;
 int main()
 {
-    int m[4], b[4], i, n = 4;
+    int b[10], i, n;
+    cout << "enter size of array (max = 10): ";
+    cin >> n; 
     for(i = 0; i < n; i++)
         {
       cout << "enter element [" << i + 1 << "] : ";
-      cin >> m[i];
-      b[i] = m[i];
+      cin >> b[i];
         }   
     cout << endl;
        
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n-1; i++)
     {
-    1<i<4;    
     if (b[i] * b[i + 1] > 0)
       { 
 	    cout<< b[i] << "," << 999 << "," ;
       }
-    else 
-      {
+    else   
+    {
         cout << b[i] << ",";
+    } 
+    }
+    
+    if (i=n)
+      {
+        cout << b[n-1];
       }
-    }    
     return 0;
 }
