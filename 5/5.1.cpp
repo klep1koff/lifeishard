0c36a8a5085ef10ab7c9098a59b25fa7f612605c
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-10 13:52:48 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -1,49 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main ()
+#define N 4 
+int main()
 {
-  int i , j, sum1, sum2, sum3;
-  
-  int a[3][3];
-    for (i = 0; i < 3; i++)
+ int  i, j, sum;
+    int matrix[N][N];
+ 
+    cout << "Matrix: " << endl;
+    for(i = 0; i < N; i++)
     {
-      for (j = 0; j < 3; j++)
-	{
-	  cout << "element [" << i + 1 << "][" << j + 1 << "] = ";
-	  cin >> a[i][j];
-	    }
+        for(j = 0; j < N; j++)
+        {
+            matrix[i][j] = rand()%(10);
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl << endl;
     }
-   cout << endl;
-   
-	    sum1 = 0;
-	    sum2 = 0;
-	    sum3 = 0;
-            for (j = 0; j < 3; j++)       
-            if (a[0][j] % 2 == 0)
-                {
-                    sum1 += a[0][j];
-                }
-            for (j = 0; j < 3; j++)    
-            if (a[1][j] % 2 == 0)
-                {
-                    sum2 += a[1][j];
-                }
-            for (j = 0; j < 3; j++)    
-            if (a[2][j] % 2 == 0)
-                {
-                    sum3 += a[2][j];
-                }    
-        cout << "summ 1 = "  <<  sum1 << " ; " << " summ 2 = "  <<  sum2 << " ; " << " summ 2 =  "  <<  sum3 << " ; " << endl; 
-     
-    if ((sum1>sum2) && (sum1>sum3))
-    cout << endl << "max summ is : "<< sum1;
+ 
     
-    if ((sum2>sum1) && (sum2>sum3))
-    cout << endl << "max summ is : "<< sum2;
-    
-    if ((sum3>sum2) && (sum3>sum1))
-    cout << endl << "max summ is : "<< sum3;
-     
-    cout << endl;
- return 0; 
+ 
+    for(j = 0; j < N; j += 1)
+    {
+        sum = 0;
+        for(i = 0; i < N; i++)
+        if (matrix[i][j] % 2 == 0)
+        {
+            sum += matrix[i][j];
+        }
+        cout << "Summ " << j + 1 << " line = " << sum << endl;
+    }
+ 
+    return 0;
 }
