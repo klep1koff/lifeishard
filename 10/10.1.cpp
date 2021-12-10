d807907cdc2c349095b49f9f933f7ef7ec4e5d2b
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-10 14:12:25 +0300

    Add files via upload

---
+++ 10/10.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	string text;
+	cout << "enter text: ";
+	cin >> text;
+
+	map<char, int> mp;
+
+	for (char ch : text)
+		mp[ch] += 1;
+
+	for (const auto &p : mp)
+		if (p.second == 1)
+		{
+			cout << p.first;
+			cout << endl;
+		}
+		else {
+			cout << "nothing absolutely ";
+		}
+
+	return 0;
+}
