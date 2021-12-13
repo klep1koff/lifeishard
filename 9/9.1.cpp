223a087d457bfbfef7d7e847b9bbdf82a30f1fe2
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-14 00:57:16 +0300

    Update 9.1.cpp

--- 9/9.1.cpp
+++ 9/9.1.cpp
@@ -1,28 +1,6 @@
-﻿#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-int main() {
-	int srav, dig_int;
-	string dig, temp;
-	string text = "9iloveduck";
-	dig = text[0];
-	dig_int = stoi(dig);
-	temp = text;
-	temp = temp.erase(0, 1);
-	srav = temp.size();
-	if (srav == dig_int)
-	{
-		cout << "True";
-	}
-	else
-	{
-		cout << "Not true";
-	}
-
-	return 0;
-}
+// 9 prog
+..
+	
 
 // 10 prog
 
