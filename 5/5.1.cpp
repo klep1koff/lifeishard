d64382179a032ac0db0ed5b01b7a6b1816f6f0b2
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-14 00:31:58 +0300

    Update 5.1.cpp

--- 5/5.1.cpp
+++ 5/5.1.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 #define N 4 
 int main()
 {
- int  i, j, sum;
+    int  i, j, sum;
+    int sum_temp = 0;
     int matrix[N][N];
- 
+
     cout << "Matrix: " << endl;
-    for(i = 0; i < N; i++)
+    for (i = 0; i < N; i++)
     {
-        for(j = 0; j < N; j++)
+        for (j = 0; j < N; j++)
         {
-            matrix[i][j] = rand()%(10);
+            matrix[i][j] = rand() % (100);
             cout << matrix[i][j] << " ";
         }
         cout << endl << endl;
     }
- 
-    
- 
-    for(j = 0; j < N; j += 1)
+
+
+
+    for (j = 0; j < N; j += 1)
     {
         sum = 0;
-        for(i = 0; i < N; i++)
-        if (matrix[i][j] % 2 == 0)
-        {
-            sum += matrix[i][j];
-        }
+        for (i = 0; i < N; i++)
+            if (matrix[i][j] % 2 == 0)
+            {
+                sum += matrix[i][j];
+            }
+        if (sum > sum_temp)
+            sum_temp = sum;
+
         cout << "Summ " << j + 1 << " line = " << sum << endl;
     }
- 
+    cout << "Max summ is : " << sum_temp << endl;
+
     return 0;
 }
