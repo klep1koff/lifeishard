944243195f8ddb1adf4ec1de899980df493d7297
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-15 01:59:00 +0300

    Update 9.1.cpp

--- 9/9.1.cpp
+++ 9/9.1.cpp
@@ -15,21 +15,16 @@ int main(int argc, char** argv) {
 string text;
 cout << "enter text: ";
 cin >> text;
-
+	
 map<char, int> mp;
-
+	
 for (char ch : text)
 mp[ch] += 1;
-
 for (const auto &p : mp)
 if (p.second == 1)
 {
 cout << p.first;
 cout << endl;    
 }
-else {
-    cout << "nothing absolutely ";
-}
-
 return 0;
 }
