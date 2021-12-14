443bc7caaf96df5069d029fdb7dea4435637bbb4
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-15 01:38:56 +0300

    Update 8.1.cpp

--- 8/8.1.cpp
+++ 8/8.1.cpp
@@ -1,23 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 int main() {
-	int srav, dig_int;
+	int sravnenie, dig_int;
 	string dig, temp;
-	string text = "9iloveduck";
+	string text = "9abobabiba";
 	dig = text[0];
 	dig_int = stoi(dig);
 	temp = text;
 	temp = temp.erase(0, 1);
-	srav = temp.size();
-	if (srav == dig_int)
+	sravnenie = temp.size();
+	if (sravnenie == dig_int)
 	{
-		cout << "Yslovie istinoe";
+		cout << "True";
 	}
 	else
 	{
-		cout << "Yslovie ne istinoe";
+		cout << "Falce";
 	}
 
 	return 0;
