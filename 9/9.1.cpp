2bea227c168b6072df039a9705a53c2652197ceb
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-16 02:05:57 +0300

    Update 9.1.cpp

--- 9/9.1.cpp
+++ 9/9.1.cpp
@@ -7,24 +7,33 @@
 #include <iostream>
 #include <string>
 #include <map>
-
+ 
 using namespace std;
-
+ 
 int main(int argc, char** argv) {
-
-string text;
-cout << "enter text: ";
-cin >> text;
-	
-map<char, int> mp;
-	
-for (char ch : text)
-mp[ch] += 1;
-for (const auto &p : mp)
-if (p.second == 1)
-{
-cout << p.first;
-cout << endl;    
-}
-return 0;
+ 
+    string text;
+    cout << "enter text: ";
+    cin >> text;
+ 
+    for (char check : text) {
+        if (!isalpha(check)) {
+            cout << "The text must not contain numbers" << endl;
+            return 0;
+        }
+    }
+ 
+    map<char, int> mp;
+ 
+    for (char ch : text)
+        mp[ch] += 1;
+ 
+    for (const auto& p : mp)
+        if (p.second == 1 )
+        {
+            cout << p.first;
+            cout << endl;
+        }
+ 
+    return 0;
 }
