31b4a17f4eae751ed03f254869341cce743b468a
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-16 00:49:38 +0300

    Update 7.1.cpp

--- 7/7.1.cpp
+++ 7/7.1.cpp
@@ -2,23 +2,26 @@
 #include <algorithm>
 using namespace std;
 int main()
-{const int N = 4;
+{
+    const int N = 4;
     int matrix[N][N];
- 
+    int i,j,k;
+    
     cout << "Original Matrix: " << endl;
-    for (int i = 0; i < N; i++)
+    for (i = 0; i < N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (j = 0; j < N; j++)
         {
             matrix[i][j] = rand() % (100);
             cout << matrix[i][j] << " ";
         }
-        cout << endl << endl;
+        cout << endl;
     }
-    for (int k = 1; k < N; k += 2) {
- 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - i - 1; j++) {
+    
+    
+    for (k = 1; k < N; k += 2) {
+        for (i = 0; i < N - 1; i++) {
+            for (j = 0; j < N - i - 1; j++) {
                 if (matrix[j][k] > matrix[j + 1][k]) {
                     // меняем элементы местами
                     int temp = matrix[j][k];
@@ -28,14 +31,14 @@ int main()
             }
         }
     }
-    cout << "Sorted Matrix: " << endl;
-    for (int i = 0; i < N; i++)
+    cout << endl<< "Sorted Matrix: " << endl;
+    for (i = 0; i < N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (j = 0; j < N; j++)
         {
  
             cout << matrix[i][j] << " ";
         }
-        cout << endl << endl;
+        cout << endl;
     }
 }
