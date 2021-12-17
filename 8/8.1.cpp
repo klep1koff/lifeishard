5f0b6706826362f5588d1ffc5ca8d8e0bd2ca403
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-17 23:09:56 +0300

    Update 8.1.cpp

--- 8/8.1.cpp
+++ 8/8.1.cpp
@@ -9,13 +9,14 @@ int main() {
 	cout << "enter text: ";
 	cin >> text;
 	dig = text[0];
+	
 	dig_int = stoi(dig);
 	temp = text;
 	temp = temp.erase(0, 1);
 	sravnenie = temp.size();
 	for (char check : temp) {
         if (!isalpha(check)) {
-            cout << "The aftertext must'nt contain numbers" << endl;
+            cout << "The aftertext mustn't contain numbers" << endl;
             return 0;
         }
     }
