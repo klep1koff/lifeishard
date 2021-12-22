8f1dc6728009c430fe85d5f77d0e2b782a525fea
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2021-12-22 18:21:04 +0300

    Update 9.1.cpp

--- 9/9.1.cpp
+++ 9/9.1.cpp
@@ -1,27 +1,25 @@
 // 9 prog
 #include <iostream>
-#include <string> 
+#include <string>
 using namespace std;
 int main()
 {
     int  start = 0, finish = 0, size = 0;
-	
-    const string a = "biba boba aboba biba";
+    string a;
+    cout << "Enter text: ";
+    getline(cin, a);
     int b = a.find_last_of(' ');
-    string last = a.substr(++b),word;
-	
-    while (start <= a.length()){
+    string last = a.substr(++b), word;
+    while (start <= a.length()) {
         finish = a.find(' ', start);
         size = finish - start;
         word = a.substr(start, size);
         start = a.find_first_not_of(' ', finish);
         if (!(word == last) && !(word.size() % 2))
             cout << word << endl;
- 
     }
-	return 0;
+    return 0;
 }
-
 // 10 prog
 
 #include <iostream>
@@ -56,4 +54,22 @@ int main(int argc, char** argv) {
         }
  
     return 0;
+}
+//2nd 10 prog 
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string a;
+    cout << "Enter text: ";
+    cin >> a;
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (a.find_first_of(a[i]) == a.find_last_of(a[i]))
+            cout << a[i];
+        else
+            cout << "";
+    }
 }
