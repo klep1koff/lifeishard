3764e6c1c0b740ec92546a4441b84fb0b5b709ad
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2022-01-22 17:08:33 +0300

    Add files via upload

---
+++ 1/7.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+#define N 4 
+int main()
+{
+	int  i, j, min;
+	int matrix[N][N];
+
+	cout << "Matrix: " << endl;
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			matrix[i][j] = rand() % (10);
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl << endl;
+	}
+
+	//    
+
+	//
+
+	return 0;
+}
