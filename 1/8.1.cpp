d1c3774166e795068a999f0d3f511d22705abb30
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2022-01-22 17:09:04 +0300

    Add files via upload

---
+++ 1/8.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+	int srav, dig_int;
+	string dig, temp;
+	string text = "9iloveduck";
+	dig = text[0];
+	dig_int = stoi(dig);
+	temp = text;
+	temp = temp.erase(0, 1);
+	srav = temp.size();
+	if (srav == dig_int)
+	{
+		cout << "Yslovie istinoe";
+	}
+	else
+	{
+		cout << "Yslovie ne istinoe";
+	}
+
+	return 0;
+}
