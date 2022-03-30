d783f5ccc11318d51aed631346cf84a32be02d9f
Stpn Klpkv <90279804+klep1koff@users.noreply.github.com>
2022-03-30 22:18:26 +0300

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void fsort(vector <string>& numb) {
+	for (int i = 0; i < numb.size(); ++i) {
+		for (int j = 0; j < numb.size() - i - 1; ++j) {
+			if (numb[j] < numb[j + 1])
+				swap(numb[j + 1], numb[j]);
+		}
+	}
+}
+
+void SplitString(string s, vector<string>& v) {
+
+	string temp = "";
+	for (int i = 0; i < s.length(); ++i) {
+
+		if (s[i] == ' ') {
+			v.push_back(temp);
+			temp = "";
+		}
+		else {
+			temp.push_back(s[i]);
+		}
+
+	}
+	v.push_back(temp);
+
+}
+
+int main()
+{
+	vector<string> strings;
+	string in;
+	cout << "enter text : \n";
+	getline(cin, in);
+	SplitString(in, strings);
+	fsort(strings);
+
+	int count = 1;
+	for (int i = 0; i < strings.size(); i++)
+	{
+		if (strings[i] == strings[i + 1])
+		{
+			count++;
+		}
+		else
+		{
+			cout << strings[i] << ":" << count << " ";
+			count = 1;
+		}
+	}
+
+}
